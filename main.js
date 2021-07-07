f943fa2ed7e1933a57c51516d679034cb583b7c2
HumbleAssassin <thaparajeev1985@gmail.com>
2021-07-07 12:13:36 +0545

    changed launch date significantly

--- main.js
+++ main.js
@@ -1,29 +1,28 @@
-let countDownDate = new Date("Jul 25, 2021 16:37:52").getTime();
+let countDownDate = new Date("Jul 25, 2050 16:37:52").getTime();
 
 function timerFunction() {
-    let now = new Date().getTime();
-    let timeRemaining = countDownDate - now;
+  let now = new Date().getTime();
+  let timeRemaining = countDownDate - now;
 
-    let days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((timeRemaining % (1000 * 60)) / (1000))
+  let days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-    let daysText = document.querySelector(".day");
-    let hoursText = document.querySelector(".hour");
-    let minutesText = document.querySelector(".minute");
-    let secondsText = document.querySelector(".second");
+  let daysText = document.querySelector(".day");
+  let hoursText = document.querySelector(".hour");
+  let minutesText = document.querySelector(".minute");
+  let secondsText = document.querySelector(".second");
 
-    daysText.innerHTML = days
-    hoursText.innerHTML = hours
-    minutesText.innerHTML = minutes
-    secondsText.innerHTML = seconds
-
-
+  daysText.innerHTML = days;
+  hoursText.innerHTML = hours;
+  minutesText.innerHTML = minutes;
+  secondsText.innerHTML = seconds;
 }
 
 timerFunction();
 
-
-setInterval(timerFunction, 1000)
-console.log(countDownDate)
+setInterval(timerFunction, 1000);
+console.log(countDownDate);
