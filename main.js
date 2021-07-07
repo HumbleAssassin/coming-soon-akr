a7ba1aa037852c039d2b772136594383ebf7c662
HumbleAssassin <thaparajeev1985@gmail.com>
2021-07-07 12:09:09 +0545

    first blood

---
+++ main.js
@@ -0,0 +1,29 @@
+let countDownDate = new Date("Jul 25, 2021 16:37:52").getTime();
+
+function timerFunction() {
+    let now = new Date().getTime();
+    let timeRemaining = countDownDate - now;
+
+    let days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeRemaining % (1000 * 60)) / (1000))
+
+    let daysText = document.querySelector(".day");
+    let hoursText = document.querySelector(".hour");
+    let minutesText = document.querySelector(".minute");
+    let secondsText = document.querySelector(".second");
+
+    daysText.innerHTML = days
+    hoursText.innerHTML = hours
+    minutesText.innerHTML = minutes
+    secondsText.innerHTML = seconds
+
+
+}
+
+timerFunction();
+
+
+setInterval(timerFunction, 1000)
+console.log(countDownDate)
